07de3968a49c9320d9e6260bdfe28525ae132ea0
660710075 <ketkorwoing_c@silpakorn.edu>
2026-03-22 19:02:54 +0700

    add mock api test

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 6 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -34,46 +34,44 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    // UI //
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      testMatch: '**/ui/*.spec.ts',
+      use:
+      {
+        ...devices['Desktop Chrome'],
+        baseURL: ''
+      },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      testMatch: '**/ui/*.spec.ts',
+      use:
+      {
+        ...devices['Desktop Firefox'],
+        baseURL: ''
+      },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      testMatch: '**/ui/*.spec.ts',
+      use:
+      {
+        ...devices['Desktop Safari'],
+        baseURL: ''
+      },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    // API //
+    {
+      name: 'webkit',
+      testMatch: '**/api/*.spec.ts',
+      use:
+      {
+        baseURL: 'https://api.escuelajs.co/api/v1/'
+      },
+    },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
