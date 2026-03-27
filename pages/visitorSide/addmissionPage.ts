4f1b48760ecd7dc81a5afd4c4861af8f6ea86ac7
660710075 <ketkorwoing_c@silpakorn.edu>
2026-03-27 07:33:18 +0700

    add mock file

--- pages/visitorSide/addmissionPage.ts
+++ pages/visitorSide/addmissionPage.ts
@@ -1,15 +1,15 @@
 import { Page, Locator, expect } from '@playwright/test';
 
 export class addmissionPage {
-  readonly page: Page;
-  readonly checkoutButton: Locator;
+  // readonly page: Page;
+  // readonly checkoutButton: Locator;
 
-  constructor(page: Page) {
-    this.page = page;
-    this.checkoutButton = page.getByRole('button',{ name: 'Checkout' });
-  }
+  // constructor(page: Page) {
+  //   this.page = page;
+  //   this.checkoutButton = page.getByRole('button',{ name: 'Checkout' });
+  // }
 
-  async clickCheckout() {
-    await this.checkoutButton.click();
-  }
+  // async clickCheckout() {
+  //   await this.checkoutButton.click();
+  // }
 }
